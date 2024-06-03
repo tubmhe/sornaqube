1748545efda9592d024ea6a1ee2798cded0526ec
tubm <tubmhe172387@fpt.edu.vn>
2024-06-04 02:14:09 +0700

    sln

--- TEst/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ TEst/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f20db77b3c500f4ad998da4d6e1e66ea4e213f3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ffe0a9d82cc673121747b53efa29303447f3024")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
